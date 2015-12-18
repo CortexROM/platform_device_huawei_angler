ebe4c898116ba6759f44a68d3e41e7d913a0bf07
Noah Greene <chevycam94@gmail.com>
2015-12-17 23:29:38 -0500

    A few tweaks

--- init.angler.power.sh
+++ init.angler.power.sh
@@ -44,18 +44,18 @@ write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
 write /sys/module/msm_performance/parameters/cpu_max_freq "4:960000 5:960000 6:960000 7:960000"
 
 # configure governor settings for little cluster
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor conservative
-restorecon -R /sys/devices/system/cpu # must restore after conservative
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/use_sched_load 1
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/timer_rate 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/hispeed_freq 960000
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/target_loads "65 460000:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/min_sample_time 40000
-write /sys/devices/system/cpu/cpu0/cpufreq/conservative/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460000:75 960000:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
 restorecon -R /sys/devices/system/cpu # must restore after setting governor
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 
@@ -63,18 +63,18 @@ write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 write /sys/devices/system/cpu/cpu4/online 1
 
 # configure governor settings for big cluster
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor conservative
-restorecon -R /sys/devices/system/cpu # must restore after conservative
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/use_sched_load 1
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/timer_rate 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/hispeed_freq 1248000
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/min_sample_time 40000
-write /sys/devices/system/cpu/cpu4/cpufreq/conservative/max_freq_hysteresis 80000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
+restorecon -R /sys/devices/system/cpu # must restore after interactive
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
 restorecon -R /sys/devices/system/cpu # must restore after setting governor
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
 
